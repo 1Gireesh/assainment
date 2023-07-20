03ed8ca2d51872a683182edabb24255bfc9f40b6
Girish vishveshvara Bhat <gireeshbhat68@gmail.com>
2023-07-20 21:07:00 +0530

    issue fixed

--- client/src/pages/MeditationResult.jsx
+++ client/src/pages/MeditationResult.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import { BiSolidUserVoice } from 'react-icons/bi';
 
-const MeditationResult = ({ generatedMeditation }) => {
-    function speech() {
-        if ('speechSynthesis' in window) {
-            const synth = window.speechSynthesis;
-            const utterance = new SpeechSynthesisUtterance(generatedMeditation.generatedText);
-
+function speech(Input) {
+    console.log(Input)
+    if ('speechSynthesis' in window) {
+        const synth = window.speechSynthesis;
+            const utterance = new SpeechSynthesisUtterance(Input);
+            
             utterance.lang = 'en-US';
             utterance.rate = 1.0;
             utterance.volume = 1.0;
             utterance.pitch = 1.0;
-
-            console.log(utterance)
+            
+            console.log(Input)
             synth.speak(utterance);
         } else {
             alert('Text-to-speech is not supported in this browser. Please try using a different browser.');
         }
     }
 
+const MeditationResult = ({ generatedMeditation }) => {
+
+
     return (
         <div className='para'>
             <h2>Generated Meditation</h2>
-            <span>
-                <BiSolidUserVoice className='btn' onClick={() => speech()} />
+            <span onClick={() => speech(generatedMeditation.generatedText)}>
+                <BiSolidUserVoice className='btn' />
                 text to speach
             </span>
             <p>{generatedMeditation.generatedText}</p>
